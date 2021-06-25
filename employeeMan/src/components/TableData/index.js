6c6152a123d5c9839fecf94340964feb3013be53
Andre Rodriguez <drae7299@gmail.com>
2021-06-25 16:42:33 -0400

    installed more modules for scripts and refactored more code

--- employeeMan/src/components/TableData/index.js
+++ employeeMan/src/components/TableData/index.js
@@ -1,59 +1,34 @@
-import React, { useReducer } from 'react';
-import Moment from 'react-moment';
-
-// What goes here?
-
-// Employess should load on page load
-// Search should filter through list of existing employees
-
+import React, { useReducer } from "react";
+import Moment from "react-moment";
 
 
 export default class TableData extends React.Component {
+  render() {
+    return (
+      <div>
+        <table className="table">
+          <tbody>
+            {this.props.employees.map((employee) => (
+              <tr>
+                <th scope="row"></th>
 
+                <td>
+                  <img src={employee.picture.medium} alt="employee" />
+                </td>
 
-
-    render() {
-        return (
-            <div>
-
-                <table className="table">
-
-
-                    <tbody>
-
-                        {/* Map Works */}
-                        {/* Moment works */}
-
-                        {this.props.employees.map((employee) => (
-                            // Key 
-                            < tr >
-                                <th scope="row"></th>
-
-                                <td>
-                                    <img
-                                        src={employee.picture.medium}
-                                        alt='employee'
-                                    />
-                                </td>
-
-
-                                <td>{employee.name.first} {employee.name.last}</td>
-                                <td>{employee.phone}</td>
-                                <td>{employee.email}</td>
-                                <Moment format="MM/DD/YYYY">
-                                    <td>{employee.dob.date}</td>
-                                </Moment>
-                            </tr>
-
-                        ))}
-
-
-                    </tbody>
-                </table>
-            </div >
-
-
-        )
-    }
+                <td>
+                  {employee.name.first} {employee.name.last}
+                </td>
+                <td>{employee.phone}</td>
+                <td>{employee.email}</td>
+                <Moment format="MM/DD/YYYY">
+                  <td>{employee.dob.date}</td>
+                </Moment>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
-
