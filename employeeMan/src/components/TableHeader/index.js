31d671ce7c31d18ab06679baf321e660bcac4bb4
Andre Rodriguez <drae7299@gmail.com>
2021-06-25 16:41:35 -0400

    working on refactoring components

--- employeeMan/src/components/TableHeader/index.js
+++ employeeMan/src/components/TableHeader/index.js
@@ -1,47 +1,24 @@
-import React from 'react';
-
-// import SortByName from '../SortByName';
+import React from "react";
 
 export default class TableHeader extends React.Component {
+  render() {
+    return (
+      <div>
+        <table className="table">
+          <thead className="thead-light">
+            <tr>
+              <th scope="col">Image</th>
 
-
-    // function SortByName() {
-    //     function handleClick(e) {
-    //         e.preventDefault();
-    //         console.log('The link was clicked!');
-    //     }
-    // }
-
-
-    // Manipulate state on App component, in my case, it would be Table Area. 
-    // Event handler goes here
-
-    render() {
-        return (
-            <div>
-                <table className="table">
-                    <thead className="thead-light">
-                        <tr>
-                            <th scope="col">Image</th>
-
-
-                            <th scope="col"
-
-                                onClick={this.props.SortByName}
-                            >
-                                Name
-
-                             </th>
-
-                            {/* <th scope="col">Name</th> */}
-                            <th scope="col">Phone</th>
-                            <th scope="col">Email</th>
-                            <th scope="col">DOB</th>
-                        </tr>
-                    </thead>
-
-                </table>
-            </div>
-        )
-    }
+              <th scope="col" onClick={this.props.SortByName}>
+                Name
+              </th>
+              <th scope="col">Phone</th>
+              <th scope="col">Email</th>
+              <th scope="col">DOB</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+    );
+  }
 }
